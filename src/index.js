16ef38a4c58b00783523a6a94e94134a3776601f
Franciso Rafael de Castro Costa <rafael.costa@febracis.local>
2020-03-03 09:21:03 -0300

    Estudo de middlewares

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use( (req, res, next) => {
+    console.log('will run before route')
+    next(new Error('Failed!'));
+})
+
+app.use((err, req, res, next) => {
+	console.log('something goes wrong');
+	res.status(500).send(err.message);
+});
+
+app.get('/', ( req, res, next ) => {
+    console.log('Route / called')
+    res.send('Hello world');
+})
+
+
+app.listen(3000, () => {
+    console.log('Server running!')
+});
